642def5d7ee5f0dc4a356fc8f706c03be90b066a
sumaaanth <sumanththota7@gmail.com>
2025-03-22 16:27:10 -0400

    implemeted the Landing page and cart icon

---
+++ ShoppingApp/Controllers/ShopController.cs
@@ -0,0 +1,34 @@
+using Microsoft.AspNetCore.Mvc;
+using ShoppingApp.Models;
+using ShoppingApp.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace MyApp.Controllers
+{
+    public class ShopController : Controller
+    {
+        private readonly ILogger<ShopController> _logger;
+
+        private readonly AppDbContext _context;
+
+        public ShopController(ILogger<ShopController> logger, AppDbContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+
+        public async Task<IActionResult> ShopIndex(Category? category)
+        {
+            _logger.LogInformation("ShopIndex action has been called.");
+
+            var products = category == null
+                ? _context.Products.ToList()
+                : _context.Products.Where(product => product.Category == category).ToList();
+
+            ViewBag.Categories = Enum.GetValues(typeof(Category)).Cast<Category>().ToList();
+            return View(products);
+        }
+
+        // Other actions...
+    }
+}

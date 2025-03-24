8e10681c0b4265a7d74cf00299b25e75482ade55
sumaaanth <sumanththota7@gmail.com>
2025-03-24 02:27:02 -0400

    enabled session over http

--- ShoppingApp/Program.cs
+++ ShoppingApp/Program.cs
@@ -26,6 +26,7 @@ builder.Services.AddDistributedMemoryCache();
 builder.Services.AddSession(options =>
 {
     options.IdleTimeout = TimeSpan.FromSeconds(1000);
+    options.Cookie.SecurePolicy = CookieSecurePolicy.None;
     options.Cookie.HttpOnly = true;
     options.Cookie.IsEssential = true;
 });
